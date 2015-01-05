df7aaa3b4b754f1497f7323a6b0c5f98e109cb62
CSAP1415 <phxbains@yahoo.com>
2015-01-04 23:36:31 -0700

    Update CellDate.java

--- src/CellDate.java
+++ src/CellDate.java
@@ -18,28 +18,30 @@ public class CellDate extends Cell {
 	}
 	
    private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
- public CellDate()
- {
- super();
- }
- public CellDate(DateFormat in)
- {
- super(in.toString());
- }
+ 	public CellDate()
+ 	{
+ 	super();
+ 	}
+ 	public CellDate(DateFormat in)
+ 	{
+ 	super(in.toString());
+ 	}
 
- public void setValue(String input)
- {
- Date date;
- try
- {
- date = dateFormat.parse(input);
- input = dateFormat.format(date);
- }
- catch (ParseException e)
- {
- input = "";
- }
- super.setValue(input);
- }
-
+ 	public void setValue(String input)
+ 	{
+ 	Date date;
+ 	try
+ 	{
+	 date = dateFormat.parse(input);
+ 	input = dateFormat.format(date);
+ 	}
+ 	catch (ParseException e)
+ 	{
+ 	input = "";
+ 	}
+ 	super.setValue(input);
+ 	}
 }
+
+
+	
