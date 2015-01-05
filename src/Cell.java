3ed44dc698ace8d8c86431f4f33abe280a258d75
CSAP1415 <phxbains@yahoo.com>
2015-01-04 23:20:59 -0700

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,1 +1,48 @@
 // placeholder
+public class Cell
+{
+ private String value;
+ public Cell()
+ {
+ value="";
+ }
+ public Cell(String in)
+ {
+ value=in;
+ }
+ public String getValue()
+ {
+ return value;
+ }
+ public String toDisplay()
+ {
+ int columnWidth = 12;
+ String output = value;
+ if(output.length()>columnWidth)
+ {
+ output=output.substring(0,columnWidth-1)+">";
+ }
+ if(output.length()%2==1)
+ {
+ output+=" ";
+ }
+ while(output.length()<columnWidth)
+ {
+ output=" "+output+" ";
+ }
+ return output;
+ }
+ public String toString()
+ {
+ String result=getValue();
+ if(result.length()==0)
+ {
+ result="<empty>";
+ }
+ return result;
+ }
+ public void setValue(String input)
+ {
+ value=input;
+ }
+}
